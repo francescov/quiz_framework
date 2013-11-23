87bf70104bd36d8f85b71b601edb02143b14d060
Francesco V <francesco@francescos-mbp.home-network>
2013-11-22 23:29:25 -0500

    IE 7 compatibility fixes
    Allows the questions multidimensional array to “play nice” with IE7
    browsers. The trailing commas in our multi-dimensional arrays don’t
    “read” properly in IE7.

--- quiz-questions-array.js
+++ quiz-questions-array.js
@@ -4,57 +4,57 @@
  *	Enter the Questions on one line. Enter each response on a separate line below, also include 1 or 0 to set as correct or wrong
  *	
  *	NOTE: Be sure to set the corresponding responses in the array below the questions. If there are 5 questions, set up 5 response rows.
- *
+ *	DOUBLE NOTE: Be sure to remove "trailing commas" since older IE browsers do not play well with them.
  **/
 var questions = [
 
 	['First Question Text',
 		['a. ',1],
 		['b. ',0],
-		['c. ',0],
+		['c. ',0]
 	],
 	['Second Question Text',
 		['a. Correct!',1],
 		['b. Wrong!',0],
-		['c. Wrong!',0],
+		['c. Wrong!',0]
 	],
 	['Third Question Text',
 		['a. Wrong!',0],
 		['b. Correct!',1],
-		['c. Wrong!',0],
+		['c. Wrong!',0]
 	],
 	['Fourth Question Text',
 		['a. Wrong!',0],
 		['b. Wrong!',0],
-		['c. Correct!',1],
+		['c. Correct!',1]
 	],
 	['Fifth Question Text',
 		['a. Correct!',1],
 		['b. Wrong!',0],
-		['c. Wrong!',0],
-	],
+		['c. Wrong!',0]
+	]
 ];
 
 /*Customize the HTML responses in this array (format is question number (numeric), correct html, incorrect html)*/
 var responses = [
 	[1,
 		'<b>Correct.</b>',
-		'<b>Sorry, that is incorrect.</b>',
+		'<b>Sorry, that is incorrect.</b>'
 	],
 	[2,
 		'<b>Correct.</b>',
-		'<b>Sorry, that is incorrect.</b>',
+		'<b>Sorry, that is incorrect.</b>'
 	],
 	[3,
 		'<b>Correct.</b>',
-		'<b>Sorry, that is incorrect.</b>',
+		'<b>Sorry, that is incorrect.</b>'
 	],	
 	[4,
 		'<b>Correct.</b>',
-		'<b>Sorry, that is incorrect.</b>',
+		'<b>Sorry, that is incorrect.</b>'
 	],
 	[5,
 		'<b>Correct.</b>',
-		'<b>Sorry, that is incorrect.</b>',
-	],	
+		'<b>Sorry, that is incorrect.</b>'
+	]
 ];
